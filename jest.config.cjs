90cccdc0c6bb51f51adb5ea1629c12a6856b5a58
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-06 17:17:48 +0100

    Install ts-jest and its types and add jest config

---
+++ jest.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/src/**/*.test.ts"],
+  resolver: "jest-ts-webcompat-resolver",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/index.ts",
+    "!src/server/index.ts",
+    "!src/server/startServer.ts",
+    "!src/loadEnvironments.ts",
+    "!src/database/connectDataBase.ts",
+  ],
+};
