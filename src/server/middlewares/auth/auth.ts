2c56e97c0e521f590b133a58a990e0a1d612e630
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-16 09:16:21 +0100

    Create auth middleware

---
+++ src/server/middlewares/auth/auth.ts
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import { type NextFunction, type Response } from "express";
+import { CustomError } from "../../../CustomError/CustomError";
+import { type CustomJwtPayload, type CustomRequest } from "../../types";
+
+const auth = (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    if (!req.header("Authorization")) {
+      const authError = new CustomError(
+        "Missing authorization header",
+        403,
+        "Missing token"
+      );
+
+      throw authError;
+    }
+
+    if (!req.header("Authorization")?.includes("Bearer")) {
+      const authError = new CustomError(
+        "Missing authorization header",
+        403,
+        "Missing token"
+      );
+
+      throw authError;
+    }
+
+    const token = req.header("Authorization")?.replace(/^Bearer\s*/i, "");
+
+    const { sub: userId } = jwt.verify(
+      token!,
+      process.env.JWT_SECRET!
+    ) as CustomJwtPayload;
+
+    req.userId = userId;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default auth;
